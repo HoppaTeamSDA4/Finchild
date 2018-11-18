07702bd98da97a6db3d92ffd75d2b0ce0eb6e0c1
tmp-sda-1162 <siqi_qian@hotmail.com>
2018-11-18 23:37:51 +0100

    complete feature of adding parent account

--- app/src/main/java/com/finchild/hoppateam/sda4/finchild/AddChildAcount.java
+++ app/src/main/java/com/finchild/hoppateam/sda4/finchild/AddChildAcount.java
@@ -1,7 +1,10 @@
 package com.finchild.hoppateam.sda4.finchild;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class AddChildAcount extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class AddChildAcount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_child_acount);
+
     }
 }
--- app/src/main/java/com/finchild/hoppateam/sda4/finchild/Settings.java
+++ app/src/main/java/com/finchild/hoppateam/sda4/finchild/Settings.java
@@ -1,16 +1,19 @@
 package com.finchild.hoppateam.sda4.finchild;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.finchild.hoppateam.sda4.finchild.adapter.ProfileSettingAdapter;
 
 public class Settings extends AppCompatActivity {
     private final String[] profile_menu={"Change Password","Add Card and Account",};
+    private TextView item_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +23,11 @@ public class Settings extends AppCompatActivity {
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                item_view= (TextView) view.findViewById(R.id.profileItem);
+                switch(item_view.getText().toString()){
+                    case "Add Card and Account":
+                        startActivity(new Intent(Settings.this, AddParentAccount.class));
+                }
             }
         });
         ProfileSettingAdapter adapter=new ProfileSettingAdapter(this,R.layout.childacc_listview_entry,profile_menu);

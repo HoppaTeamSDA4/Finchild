034c23f06d5a5d5e5cc927763c536566fddca55d
tmp-sda-1162 <siqi_qian@hotmail.com>
2018-11-22 17:39:43 +0100

    data recieving on feature of'Child Account' completed

--- app/src/main/java/com/finchild/hoppateam/sda4/finchild/HomeActivity.java
+++ app/src/main/java/com/finchild/hoppateam/sda4/finchild/HomeActivity.java
@@ -1,24 +1,30 @@
 package com.finchild.hoppateam.sda4.finchild;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+
 import com.finchild.hoppateam.sda4.finchild.adapter.ChildAdapter;
 import com.finchild.hoppateam.sda4.finchild.login.LoginActivity;
-import com.finchild.hoppateam.sda4.finchild.modules.ChildDetails;
+import com.finchild.hoppateam.sda4.finchild.modules.ChildAccount;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
     private FirebaseAuth firebaseAuth;
@@ -27,7 +33,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnAddChild;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter adapter;
-    List<ChildDetails> children = new ArrayList<>();
+    private List<ChildAccount> childAccList = new ArrayList<>();
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,54 +42,56 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_home);
         //initializing firebase authentication object
         firebaseAuth = FirebaseAuth.getInstance();
-
-        backBtn=(ImageView) findViewById(R.id.ivBack);
-        btnSettings = (ImageView) findViewById(R.id.ivSettings);
-        btnAddChild= (Button) findViewById(R.id.btnAddChild);
-        backBtn.setOnClickListener(this);
+        //if the user is not logged in
+        //that means current user will return null
+         FirebaseUser user = firebaseAuth.getCurrentUser(); 
+        if (firebaseAuth.getCurrentUser() == null) {
+            //closing this activitybtnSettings = (ImageView) findViewById(R.id.ivSettings);
+            finish();
+            //starting login activitybackBtn.setOnClickListener(this);
+            startActivity(new Intent(this, LoginActivity.class));
+        } else{
+            userId = user.getUid();
+        }
+        btnSettings=(ImageView) findViewById(R.id.ivSettings);
         btnSettings.setOnClickListener(this);
+        btnAddChild = (Button) findViewById(R.id.btnAddChild);
+        btnSettings.setOnClickListener(this);
+        backBtn = (ImageView) findViewById(R.id.ivBack);
         btnAddChild.setOnClickListener(this);
-        //purchases = new ArrayList<Purchase_Details>();
+
         initialiseData();
-
-
-        //Create recycler view
         mRecyclerView = (RecyclerView) findViewById(R.id.childrenList);
         // create an adapter and supply the data to be displayed
-        adapter = new ChildAdapter(this, children);
+        adapter = new ChildAdapter(this, childAccList);
         //Connect the adapter with RecyclerView
         mRecyclerView.setAdapter(adapter);
         //Give the recycler view a default layout manager
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        //if the user is not logged in
-        //that means current user will return null
-        if(firebaseAuth.getCurrentUser() == null){
-            //closing this activity
-            finish();
-            //starting login activity
-            startActivity(new Intent(this, LoginActivity.class));
-        }
+    }
 
-        //getting current user
-        FirebaseUser user = firebaseAuth.getCurrentUser();
+    @Override
+    protected void onStart() {
+        super.onStart();
+
     }
 
     public void onClick(View view) {
         //if btnSettings is pressed
 
-        if(view==btnAddChild){
-             Intent settingsIntent = new Intent(this, AddChildAcount.class);
-             // Start the activity.                                                  
-             startActivity(settingsIntent);                                          
-                                                                                     
+        if (view == btnAddChild) {
+            Intent settingsIntent = new Intent(this, AddChildAcount.class);
+            // Start the activity.
+            startActivity(settingsIntent);
+
         }
 
-        if(view==btnSettings) {
+        if (view == btnSettings) {
             startActivity(new Intent(this, Settings.class));
         }
 
-        if(view==backBtn){
+        if (view == backBtn) {
             firebaseAuth.signOut();
             //closing activity
             finish();
@@ -92,18 +101,52 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     // Method of Initiating Data in the list, to be called for the RecyclerView
-    public void initialiseData(){
+    public void initialiseData() {
+        DatabaseReference accountRef = FirebaseDatabase.getInstance().getReference().child("account").child(userId);
+        accountRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String ParentAccountNo = "";
+                for (DataSnapshot accshot : dataSnapshot.getChildren()) {
+                    ParentAccountNo = accshot.getKey();
+                    System.out.println(ParentAccountNo);
+                }
+                if (!ParentAccountNo.equals("") && ParentAccountNo != null) {
+                    DatabaseReference childAccRef = FirebaseDatabase.getInstance().getReference().child("child").child(ParentAccountNo);
+                    childAccRef.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            childAccList.clear();
+                            for (DataSnapshot childAccShot : dataSnapshot.getChildren()) {
+                                ChildAccount childAccount = childAccShot.getValue(ChildAccount.class);
+                                childAccList.add(childAccount);
 
-        children.add(new ChildDetails("Gloria", "2500 Kr", 0));
-        children.add(new ChildDetails("Adam", "1000 Kr", 1));
-        children.add(new ChildDetails("Gloria", "2500 Kr", 0));
-        children.add(new ChildDetails("Adam", "1000 Kr", 1));
-        children.add(new ChildDetails("Gloria", "2500 Kr", 0));
-        children.add(new ChildDetails("Adam", "1000 Kr", 1));
-        children.add(new ChildDetails("Gloria", "2500 Kr", 0));
-        children.add(new ChildDetails("Adam", "1000 Kr", 1));
-        children.add(new ChildDetails("Gloria", "2500 Kr", 0));
-        children.add(new ChildDetails("Adam", "1000 Kr", 1));
+                            }
+                            mRecyclerView = (RecyclerView) findViewById(R.id.childrenList);
+                            // create an adapter and supply the data to be displayed
+                            adapter = new ChildAdapter(HomeActivity.this, childAccList);
+                            //Connect the adapter with RecyclerView
+                            mRecyclerView.setAdapter(adapter);
+                            //Give the recycler view a default layout manager
+                            mRecyclerView.setLayoutManager(new LinearLayoutManager(HomeActivity.this));
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
     }
+
+
 }

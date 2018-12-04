24969804730451604cec628dd0be48642a7178dc
Dany Kassdaoud <danydaood@yahoo.com>
2018-12-04 10:58:31 +0100

    Managing spending limits

--- app/src/main/java/com/finchild/hoppateam/sda4/finchild/ChildSpendLimit.java
+++ app/src/main/java/com/finchild/hoppateam/sda4/finchild/ChildSpendLimit.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -29,9 +30,13 @@ public class ChildSpendLimit extends AppCompatActivity {
     private EditText amountSpendLimits;
     private Spinner frequency;
     private String limitType;
-    private String limitAmount;
-    private Session session;
-
+    private double limitAmount;
+    //private TextView limitMessage;
+    //private Session session;
+    private CheckBox dailycheck;
+    private CheckBox weeklycheck;
+    private CheckBox monthlycheck;
+    private Button resetSpendingLimits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,45 +46,178 @@ public class ChildSpendLimit extends AppCompatActivity {
 
         //initializing views
         btnBack = (ImageView) findViewById(R.id.btnBack);
-        confirm=(Button) findViewById(R.id.confirm);
-        frequency= (Spinner) findViewById(R.id.spinnerFrequency);
-        amountSpendLimits=(EditText) findViewById(R.id.amountSpendLimit);
+        confirm = (Button) findViewById(R.id.confirm);
+        frequency = (Spinner) findViewById(R.id.spinnerFrequency);
+        amountSpendLimits = (EditText) findViewById(R.id.amountSpendLimit);
+        //limitMessage=(TextView) findViewById(R.id.statusLimitMessage);
+        dailycheck = (CheckBox) findViewById(R.id.dailyCheckBox);
+        weeklycheck = (CheckBox) findViewById(R.id.weeklyCheckBox);
+        monthlycheck = (CheckBox) findViewById(R.id.monthlyCheckBox);
+        resetSpendingLimits = (Button) findViewById(R.id.resetLimits);
 
         //Create adapter to frequency spinner
-         final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
-                (this,R.array.limit,android.R.layout.simple_spinner_item);
+        final ArrayAdapter <CharSequence> adapter = ArrayAdapter.createFromResource
+                (this, R.array.limit, android.R.layout.simple_spinner_item);
+
         //initialize spinner
         frequency.setAdapter(adapter);
+
+        //initialize Session
+        final Session session = new Session(ChildSpendLimit.this);
+
+        //initialize DatabaseReference
+        final DatabaseReference accountRef = FirebaseDatabase.getInstance().getReference().child("child").child(session.getParentAcc());
+
+        //Geting the spinner value
         frequency.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView <?> parent, View view, int position, long id) {
-                limitType=frequency.getItemAtPosition(position).toString();
+                limitType = frequency.getItemAtPosition(position).toString();
+                accountRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot accountShot : dataSnapshot.getChildren()) {
+                            if (accountShot.child("name").getValue().toString().equals(session.getChildName())) {
+                                amountSpendLimits.setText(accountShot.child(limitType.toLowerCase() + "LimitAmount").getValue().toString());
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
             }
 
             @Override
             public void onNothingSelected(AdapterView <?> parent) {
-                limitType=null;
+                limitType = null;
             }
         });
 
-        confirm.setOnClickListener(new View.OnClickListener() {
+        accountRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot accountShot : dataSnapshot.getChildren()) {
+                    if (accountShot.child("name").getValue().toString().equals(session.getChildName())) {
+                       /* String daily = "Daily: " + accountShot.child("dailyLimitAmount").getValue().toString();
+                        String weekly = "Weekly: " + accountShot.child("weeklyLimitAmount").getValue().toString();
+                        String monthly = "Monthly: " + accountShot.child("monthlyLimitAmount").getValue().toString();*/
+
+                   /*     //Check if there is no spending limit
+                        if(!Boolean.valueOf(accountShot.child("dailyLimit").getValue().toString())&&
+                           !Boolean.valueOf(accountShot.child("weeklyLimit").getValue().toString())&&
+                           !Boolean.valueOf(accountShot.child("monthlyLimit").getValue().toString()))
+                            limitMessage.setText("There is no spending limits.");
+                        // check if there are spending limits
+                        else limitMessage.setText("The spending limits are:\n"+
+                                displayLimit(accountShot.child("dailyLimit").getValue().toString(),daily)+
+                                displayLimit(accountShot.child("weeklyLimit").getValue().toString(),weekly)+
+                                displayLimit(accountShot.child("monthlyLimit").getValue().toString(),monthly));*/
+                        if (accountShot.child("dailyLimit").getValue().toString().equals("true")) {
+                            dailycheck.setVisibility(View.VISIBLE);
+                            dailycheck.setText("Daily: " + accountShot.child("dailyLimitAmount").getValue().toString() + " kr");
+                        }
+                        if (Boolean.valueOf(accountShot.child("weeklyLimit").getValue().toString())) {
+                            weeklycheck.setVisibility(View.VISIBLE);
+                            weeklycheck.setText("Weekly: " + accountShot.child("weeklyLimitAmount").getValue().toString() + " kr");
+                        }
+                        if (Boolean.valueOf(accountShot.child("monthlyLimit").getValue().toString())) {
+                            monthlycheck.setVisibility(View.VISIBLE);
+                            monthlycheck.setText("Monthly: " + accountShot.child("monthlyLimitAmount").getValue().toString() + " kr");
+                        }
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        resetSpendingLimits.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                limitAmount=amountSpendLimits.getText().toString();
-                final Session session = new Session(ChildSpendLimit.this);
-                final DatabaseReference accountRef = FirebaseDatabase.getInstance().getReference().child("child").child(session.getParentAcc());
                 accountRef.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         for (DataSnapshot accountShot : dataSnapshot.getChildren()) {
                             if (accountShot.child("name").getValue().toString().equals(session.getChildName())) {
                                 String childAccounKey = accountShot.getKey();
-                                if (TextUtils.isEmpty(limitAmount)) {
+                                if (dailycheck.isChecked()) {
+                                    accountRef.child(childAccounKey).child("dailyLimitAmount").setValue(0);
+                                    accountRef.child(childAccounKey).child("dailyLimit").setValue(false);
+                                    dailycheck.setChecked(false);
+                                    dailycheck.setVisibility(View.GONE);
+                                }
+                                if (weeklycheck.isChecked()) {
+                                    accountRef.child(childAccounKey).child("weeklyLimitAmount").setValue(0);
+                                    accountRef.child(childAccounKey).child("weeklyLimit").setValue(false);
+                                    weeklycheck.setChecked(false);
+                                    weeklycheck.setVisibility(View.GONE);
+                                }
+                                if (monthlycheck.isChecked()) {
+                                    accountRef.child(childAccounKey).child("monthlyLimitAmount").setValue(0);
+                                    accountRef.child(childAccounKey).child("monthlyLimit").setValue(false);
+                                    monthlycheck.setChecked(false);
+                                    monthlycheck.setVisibility(View.GONE);
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
+
+        //Setting the limit in database
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                limitAmount = Double.valueOf(amountSpendLimits.getText().toString());
+
+                accountRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot accountShot : dataSnapshot.getChildren()) {
+                            if (accountShot.child("name").getValue().toString().equals(session.getChildName())) {
+                                String childAccounKey = accountShot.getKey();
+                                if (limitAmount == 0) {
                                     Toast.makeText(ChildSpendLimit.this, "Please enter the amount", Toast.LENGTH_LONG).show();
                                     return;
                                 }
-                                //accountRef.child(childAccounKey).child("limitAmount").setValue(limitAmount);
-                                //accountRef.child(childAccounKey).child("limitType").setValue(limitType);
+
+                                if (TextUtils.isEmpty(limitType)) {
+                                    Toast.makeText(ChildSpendLimit.this, "Please enter the type of the limit", Toast.LENGTH_LONG).show();
+                                    return;
+                                }
+
+                                String limitField = selectSwitchLimit(limitType);
+
+
+                                accountRef.child(childAccounKey).child(limitField + "Amount").setValue(limitAmount);
+                                accountRef.child(childAccounKey).child(limitField).setValue(true);
+                                if (limitType.equals("Daily")) {
+                                    dailycheck.setChecked(false);
+                                    dailycheck.setVisibility(View.VISIBLE);
+                                    dailycheck.setText(limitType + ": " + limitAmount + " kr");
+                                } else if (limitType.equals("Weekly")) {
+                                    weeklycheck.setChecked(false);
+                                    weeklycheck.setVisibility(View.VISIBLE);
+                                    weeklycheck.setText(limitType + ": " + limitAmount + " kr");
+                                } else if (limitType.equals("Monthly")) {
+                                    monthlycheck.setChecked(false);
+                                    monthlycheck.setVisibility(View.VISIBLE);
+                                    monthlycheck.setText(limitType + ": " + limitAmount + " kr");
+                                }
                             }
                         }
                     }
@@ -94,9 +232,10 @@ public class ChildSpendLimit extends AppCompatActivity {
         });
 
     }
+
     public void onClick(View view) {
         //if btnBack is pressed
-        if (view == btnBack){
+        if (view == btnBack) {
             // Create an Intent to start the control activity
             Intent backIntent = new Intent(this, Control.class);
             // Start the activity.
@@ -105,18 +244,18 @@ public class ChildSpendLimit extends AppCompatActivity {
 
     }
 
-    public String selectSwitchLimit(String limitType){
+    public String selectSwitchLimit(String limitType) {
 
-        String limitField=null;
-        switch(limitType){
+        String limitField = null;
+        switch (limitType) {
             case "Daily":
-                limitField="dailyLimit";
+                limitField = "dailyLimit";
                 break;
-            case"weekly":
-                limitField="weeklyLimit";
+            case "Weekly":
+                limitField = "weeklyLimit";
                 break;
-            case"Monthly":
-                limitField="monthlyLimit";
+            case "Monthly":
+                limitField = "monthlyLimit";
                 break;
             default:
                 break;
@@ -124,4 +263,10 @@ public class ChildSpendLimit extends AppCompatActivity {
 
         return limitField;
     }
+
+    public String displayLimit(String status, String result) {
+        if (!Boolean.valueOf(status)) return "";
+        else return result + " kr\n";
+    }
+
 }
